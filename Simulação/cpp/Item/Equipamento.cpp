c8f8acca208e268b54e2508014a9a2cd54a08586
Sairaf <abnergillet16@gmail.com>
2014-11-27 13:44:41 -0300

    Update Equipamento.cpp

--- Simulação/cpp/Item/Equipamento.cpp
+++ Simulação/cpp/Item/Equipamento.cpp
@@ -1,31 +1,54 @@
-#include <string>
-#include <vector>
-#include <list>
-#include <iostream>
-#include <assert.h>
-
 #include "Equipamento.h"
 
-namespace java
-{
-namespace Item
-{
 
-void Equipamento::Equipamento()
+void Equipamento::Equipamento(int b_Atk)
+:Item()
 {
+ this->bonus_Atk = b_Atk;
+ this->bonus_Def = 0;
+ this->bonus_Mag = 0;
+ this->bonus_Vel = 0;
+ this->Efeito_elemental = NULL;	
 }
 
-java::lang::Void Equipamento::Equipamento(java::lang::String nome, java::lang::String descricao, int b_Atk, int b_Def, int b_Mag, int b_Vel)
+void Equipamento::Equipamento(int b_Def)
+:Item()
 {
-	return 0;
+ this->bonus_Atk = 0;
+ this->bonus_Def = b_Def;
+ this->bonus_Mag = 0;
+ this->bonus_Vel = 0;
+ this->Efeito_elemental = NULL;	
 }
 
-void Equipamento::Add_Efeito(java::Elemento::Elemento elemento)
+
+Equipamento::Equipamento(const string& nome,const string& descricao, int b_Atk, int b_Def, int b_Mag, int b_Vel, const Elemento& elemento)
+:Item(nome, descricao)
 {
+ this->bonus_Atk = b_Atk;
+ this->bonus_Def = b_Def;
+ this->bonus_Mag = b_Mag;
+ this->bonus_Vel = b_Vel;
+ this->Efeito_elemental = elemento;	
 }
 
-void Equipamento::Equipamento()
-{
+void Equipamento::~Equipamento(){
 }
-}  // namespace Item
-}  // namespace java
+
+
+void Equipamento::Equipamento(const Equipamento& equipamento_Cpy)
+:Item(equipamento_Cpy)
+{
+ this->bonus_Atk = equipamento_Cpy.bonus_Atk;
+ this->bonus_Def = equipamento_Cpy.bonus_Def;
+ this->bonus_Mag = equipamento_Cpy.bonus_Mag;
+ this->bonus_Vel = equipamento_Cpy.bonus_Vel;
+ this->Efeito_elemental = equipamento_Cpy.Efeitoo_elemental;	
+}
+
+ostream& operator<<(ostream&, const Equipamento& equipamento){
+ output << <static_cast> <const Item&> (equipamento) << "\n";
+ output << "Poder de ataque:" equipamento.getAtk() << endl;
+ output << "Poder de ataque:" equipamento.getAtk() << endl;
+}
+
