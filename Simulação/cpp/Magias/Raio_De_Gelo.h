429cd75c73e76660af9379870b94b97d9c079989
Sairaf <abnergillet16@gmail.com>
2014-11-27 15:30:33 -0300

    Update Raio_De_Gelo.h

--- Simulação/cpp/Magias/Raio_De_Gelo.h
+++ Simulação/cpp/Magias/Raio_De_Gelo.h
@@ -13,8 +13,8 @@ class Raio_De_gelo: public Magia
 {
 public:
 	void Raio_De_Gelo(string nome = "Raio de Gelo", int poder = 20);
-	void Magia(const string&, const int&, const string&, const Elemento&);
-	void Magia();
+	void Raio_De_Gelo(const string&, const int&, const string&, const Elemento&);
+	void Raio_De_Gelo();
 
 	void Add_Poder(const int&);
 };
