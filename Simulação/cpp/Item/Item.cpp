c2962f517a23da3fb852ddde8fd8c8780bc362c8
Sairaf <abnergillet16@gmail.com>
2014-11-27 14:11:28 -0300

    Update Item.cpp

--- Simulação/cpp/Item/Item.cpp
+++ Simulação/cpp/Item/Item.cpp
@@ -1,26 +1,49 @@
-#include <string>
-#include <vector>
-#include <list>
-#include <iostream>
-#include <assert.h>
-
 #include "Item.h"
 
-namespace java
-{
-namespace Item
-{
 
-void Item::Item()
+Item::Item(string nome, string descricao)
+:nome_Item(nome), descricao_Item(descricao)
+{
+  this->durabilidade = 5;
+}
+
+Item::Item(const string& nome,const string& descricao, const int& durabilidade)
+{
+  this->nome_Item = nome;
+  this->descricao_Item = descricao;
+  this->durabilidade = durabilidade;
+}
+
+Item::~Item()
 {
 }
 
-void Item::Item(java::lang::String nome, java::lang::String descricao)
-{
+Item::Item(const Item& item_Cpy){
+  this->nome_Item = item_Cpy.nome_Item;
+  this->descricao_Item = item_Cpy.descricao;
+  this->durabilidade = item_Cpy.durabilidade;
 }
 
-void Item::Item()
-{
+void Item::set_Nome_Item(const string& nome){
+  if(nome.empty() == false)
+   this->nome_Item = nome;
+  else
+   this->nome_Item = "Desconhecido";
 }
-}  // namespace Item
-}  // namespace java
+void Item::set_Descricao_Item(const string&descricao){
+  if(descricao.empty() == false)
+   this->descricao_Item = item_Cpy.descricao;
+  else
+   this->descricao_Item = "Desconhecido";
+}
+
+void Item::set_Durabilidade(const int& durabilidade){
+  if(durabilidade >= 1)
+   this->durabilidade = item_Cpy.durabilidade;
+  else
+    this->durabilidade = 1;
+}
+
+ostream& operator<<(ostream& output, const Item& item){
+ output << "Nome do item" : item.get_Nome_Item() << endl;
+}
