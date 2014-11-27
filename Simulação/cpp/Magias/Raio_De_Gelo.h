a2f90d212a7be21d483832094e551da32e2daf43
Sairaf <abnergillet16@gmail.com>
2014-11-27 14:24:25 -0300

    Update Raio_De_Gelo.h

--- Simulação/cpp/Magias/Raio_De_Gelo.h
+++ Simulação/cpp/Magias/Raio_De_Gelo.h
@@ -1,5 +1,5 @@
-#ifndef MAGIA_H
-#define MAGIA_H
+#ifndef RAIO_DE_GELO_H
+#define RAIO_DE_GELO_H
 
 #include <string>
 #include <vector>
@@ -7,32 +7,16 @@
 #include <iostream>
 #include <assert.h>
 
-#include "Elemento.h"
+#include "Magia.h"
 
-namespace java
-{
-namespace Raio_De_gelo
-{
 class Raio_De_gelo: public Magia
-protected:
-	string nome_Magia;
-
-	int poder_Magia;
-
-	string descricao_Magia;
-
-	Elemento elemento;
-	
-
+{
 public:
-	void Magia();
-
+	void Raio_De_Gelo(string nome = "Raio de Gelo", int poder = 20);
 	void Magia(string nome, int poder_Magia, string descricao, Elemento elemento);
-
 	void Magia();
 
+	void Add_Poder(const int&);
 };
 
-}  // namespace Magias
-}  // namespace java
-#endif
+
