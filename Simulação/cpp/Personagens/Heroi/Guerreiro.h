57a89f134ee46adb5478310375449ce0ccc538a5
Sairaf <abnergillet16@gmail.com>
2014-11-27 14:25:52 -0300

    Update Guerreiro.h

--- Simulação/cpp/Personagens/Heroi/Guerreiro.h
+++ Simulação/cpp/Personagens/Heroi/Guerreiro.h
@@ -1,5 +1,5 @@
-#ifndef JAVA_PERSONAGENS_HEROI_GUERREIRO_H
-#define JAVA_PERSONAGENS_HEROI_GUERREIRO_H
+#ifndef GUERREIRO_H
+#define GUERREIRO_H
 
 #include <string>
 #include <vector>
@@ -11,32 +11,20 @@
 #include "Magia.h"
 #include "Heroi.h"
 
-namespace java
-{
-namespace Personagens
-{
-namespace Heroi
-{
+
 class Guerreiro : public Heroi
 {
 protected:
 	Magia Tecnica;
-
-
 public:
-	void Adicionar_Tecnica(Magia magia);
 
 	void Guerreiro();
-
 	void Guerreiro(string nome, int hp, int p, int ataque, int defesa, int magia, int velocidade, int maxHp, int maxMp);
-
+	
+	void Adicionar_Tecnica(Magia magia);
 	void Equipar(const Equipamento&);
-
 	void Guerreiro();
 
 };
 
-}  // namespace Heroi
-}  // namespace Personagens
-}  // namespace java
 #endif
