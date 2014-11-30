05a0c1b3d4ecf0a54cd2fcfb180fdd00e1c94191
Sairaf <abnergillet16@gmail.com>
2014-11-30 00:49:42 -0400

    Update Grupo.h

--- Simulação/Main/RPG_CPP/Grupo.h
+++ Simulação/Main/RPG_CPP/Grupo.h
@@ -1,1 +1,33 @@
+#include <cstlib>
+#include <iostream>
+#include <stdlib.h>
+
+#include "Heroi.h"
+#include "Item.h"
+#include <vector>
+
+using namespace std;
+
+class Grupo:
+{
+  friend ostream& operator<<(ostream&, const Grupo&);
+  protected:
+    vector <Heroi*> grupo;
+    vector <Item*> inventario;
+  public:
+    Grupo();
+    Grupo(const Grupo&);
+    ~Grupo();
+    
+    void set_Heroi(const Heroi&);
+    void set_Item(const Item&);
+    
+    <Heroi> get_Heroi() const;
+    <Item> get_Item() const;
+    
+    
+    void Menu();
+    
+  
+}
 
